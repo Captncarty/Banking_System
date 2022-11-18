17639cff19b52ab4b80548dc5f8c977e3449bb79
kelvin <edithmukoro3@gmail.com>
2022-11-18 09:32:39 +0100

    parameters

---
+++ param.c
@@ -0,0 +1,37 @@
+#include "bank.h"
+
+/**
+  * interest - rate of interest
+  * @t: time
+  * @amount: initial ammount
+  * @rate: value
+  */
+
+float interest(float t,float amount,int rate)
+{
+	float SI;
+	SI = (rate * t * amount) / 100.0;
+	return (SI);
+}
+
+/**
+  * fordelay - sequencing
+  * @j: intger to print
+  */
+
+void fordelay(int j)
+{   int i,k;
+    for(i=0;i<j;i++)
+         k=i;
+}
+
+/**
+  * close - remarks
+  */
+
+void close(void)
+{
+    printf("\n\n\n\nThis C Mini Project is developed by Code With C team!");
+}
+
+
